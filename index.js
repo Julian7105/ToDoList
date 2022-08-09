565f97a9d1cceb1950a1dd53c16b7d573ce9b5d6
Julian7105 <julianpaullucien@gmail.com>
2022-08-09 22:07:57 +0200

    Version finale

--- index.js
+++ index.js
@@ -7,52 +7,42 @@ let index = 0;
 let todoText = "";
 
 input.addEventListener("change", function (e) {
-    todoText = e.target.value;
+  todoText = e.target.value;
 });
-
 
 function addTodo() {
+  if (todoText !== "") {
+    index += 1;
 
-    if (todoText !== ""){
+    const barre = document.createElement("div");
+    barre.classList.add("list");
+    cadreList.appendChild(barre);
 
-        index += 1;
-        
-        const barre = document.createElement("div");
-        barre.classList.add("list");
-        cadreList.appendChild(barre);
-        
-        const tiret = document.createElement("span");
-        tiret.textContent = "-";
-        tiret.classList.add("tiret");
-        barre.appendChild(tiret);
-        
-        const txt = document.createElement("span");
-        txt.textContent = todoText;
-        txt.classList.add("text");
-        barre.appendChild(txt);
-        
-        
-        const poubelle = document.createElement("i");
-        poubelle.classList.add("fa-solid");
-        poubelle.classList.add("fa-trash-can");
-        barre.appendChild(poubelle);
-        
-        
-        
-        if (index > 0){
-            poubelle.addEventListener("click", () =>{
-                barre.remove();
-            })
-        }
-        
+    const tiret = document.createElement("span");
+    tiret.textContent = "-";
+    tiret.classList.add("tiret");
+    barre.appendChild(tiret);
+
+    const txt = document.createElement("span");
+    txt.textContent = todoText;
+    txt.classList.add("text");
+    barre.appendChild(txt);
+
+    const poubelle = document.createElement("i");
+    poubelle.classList.add("fa-solid");
+    poubelle.classList.add("fa-trash-can");
+    barre.appendChild(poubelle);
+
+    if (index > 0) {
+      poubelle.addEventListener("click", () => {
+        barre.remove();
+      });
     }
-    
+    todoText = "";
+  }
 }
 
-ajouter.addEventListener("click", () =>{
-    addTodo();
-    input.value = "";
+ajouter.addEventListener("click", () => {
+  addTodo();
+  input.value = "";
 });
-
-
-
